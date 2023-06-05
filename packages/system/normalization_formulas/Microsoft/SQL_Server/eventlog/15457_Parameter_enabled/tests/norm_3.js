9802abdd7a5f7916009e6c4939ee44c6cca545b5
Kirill Kiryanov <22522260+zBlurr@users.noreply.github.com>
2023-06-05 20:30:14 +0300

    DE_xp_cmdshell_enabled_MSSQL_EID_15457.evtx (#297)
    * refactoring norm formula
    * add rule

---
+++ packages/system/normalization_formulas/Microsoft/SQL_Server/eventlog/15457_Parameter_enabled/tests/norm_3.js
@@ -0,0 +1,18 @@
+{
+  "subject": "account",
+  "action": "modify",
+  "object": "configuration",
+  "status": "success",
+  "event_src.category": "Database server",
+  "event_src.hostname": "desktop-gb13g7p",
+  "event_src.subsys": "Application",
+  "event_src.title": "sql_server",
+  "event_src.vendor": "microsoft",
+  "id": "PT_Microsoft_SQL_Server_eventlog_15457_Parameter_enabled",
+  "importance": "low",
+  "msgid": "15457",
+  "object.property": "parameter",
+  "object.value": "Ad Hoc Distributed Queries",
+  "reason": "parameter enabled",
+  "time": "2020-01-31T13:46:33Z"
+}
---
+++ packages/system/normalization_formulas/Microsoft/SQL_Server/eventlog/15457_Parameter_enabled/tests/norm_4.js
@@ -0,0 +1,18 @@
+{
+  "subject": "account",
+  "action": "modify",
+  "object": "configuration",
+  "status": "success",
+  "event_src.category": "Database server",
+  "event_src.hostname": "desktop-gb13g7p",
+  "event_src.subsys": "Application",
+  "event_src.title": "sql_server",
+  "event_src.vendor": "microsoft",
+  "id": "PT_Microsoft_SQL_Server_eventlog_15457_Parameter_enabled",
+  "importance": "low",
+  "msgid": "15457",
+  "object.property": "parameter",
+  "object.value": "cross db ownership chaining",
+  "reason": "parameter enabled",
+  "time": "2020-01-31T13:53:36Z"
+}
